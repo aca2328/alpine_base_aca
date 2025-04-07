2e3f18539dffde90305d84240e9520e2d8ec3f55
Antoine Camerlo <aca2328@gmail.com>
2025-04-07 23:14:36 +0200

    adapt run script to multi-arch

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-docker build --no-cache -t alpaca .
 docker container prune -f
-docker run --name alpaca -v "$PWD/:/home:rw" -v /var/run/docker.sock:/var/run/docker.sock -ti -d alpaca
+docker run --name alpaca -v "$PWD/:/home:rw" -v /var/run/docker.sock:/var/run/docker.sock -ti -d aca2328/alpaca:multi-arch
